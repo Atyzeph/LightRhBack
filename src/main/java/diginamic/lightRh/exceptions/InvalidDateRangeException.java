3a5a0458e7d7660df9aab78744174bab1ed33ee3
AtyZeph <bennetot-clm@hotmail.com>
2023-09-20 15:42:55 +0200

    Les exceptions ne s'affichent pas en console. Voir pour implémenter Log4j

---
+++ src/main/java/diginamic/lightRh/exceptions/InvalidDateRangeException.java
@@ -0,0 +1,8 @@
+package diginamic.lightRh.exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidDateRangeException extends RuntimeException {
+	public InvalidDateRangeException(String s) {
+		super(s);
+	}
+}
